0106ab0469108daacccfa42fe46237363be6ff13
Jealous Matsikachando <pasej5@gmail.com>
2023-08-31 01:59:38 -0700

    perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,29 +8,27 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+    if (tree == NULL)
+        return 0;
 
-	if (tree == NULL)
-		return 0;
+    size_t left_height = binary_tree_height(tree->left);
+    size_t right_height = binary_tree_height(tree->right);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return 1 + (left_height > right_height ? left_height : right_height);
+    return 1 + (left_height > right_height ? left_height : right_height);
 }
 
 /**
- * binary_tree_size - Measures the size of a binary tree.
- * @tree: Pointer to the root node of the tree to measure.
+ * binary_tree_count_nodes - Counts the number of nodes in a binary tree.
+ * @tree: Pointer to the root node of the tree to count nodes.
  *
- * Return: Size of the tree, or 0 if tree is NULL.
+ * Return: Number of nodes in the tree, or 0 if tree is NULL.
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_count_nodes(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return 0;
+    if (tree == NULL)
+        return 0;
 
-	return 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+    return 1 + binary_tree_count_nodes(tree->left) + binary_tree_count_nodes(tree->right);
 }
 
 /**
@@ -41,11 +39,12 @@ size_t binary_tree_size(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return 0;
+    if (tree == NULL)
+        return 0;
 
-	size_t height = binary_tree_height(tree);
-	size_t size = binary_tree_size(tree);
+    size_t height = binary_tree_height(tree);
+    size_t nodes = binary_tree_count_nodes(tree);
 
-	return (size == ((size_t)1 << height) - 1);
+    return (nodes == ((size_t)1 << height) - 1);
 }
+
